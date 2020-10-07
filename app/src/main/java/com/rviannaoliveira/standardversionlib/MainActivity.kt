97d0f3458f2218e0b8f8bef7bb569a10ef6aeb66
Rodrigo Vianna Calixto de Oliveira <rodrigo.calixto@xpi.com.br>
2020-10-07 17:24:18 -0300

    first commit

---
+++ app/src/main/java/com/rviannaoliveira/standardversionlib/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.rviannaoliveira.standardversionlib
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
